2254ac8373290bafc74ce5aaadd83ea284730991
kimptgr <patingrekim@gmail.com>
2026-04-22 13:18:14 +0200

    ajout route pour nouveau

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {Nouveau} from './livres/component/nouveau/nouveau';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'livres/nouveau', component: Nouveau }
+];

eb5e9f9c06f3105c9d65b2cc1861a27a16112e3a
kimptgr <patingrekim@gmail.com>
2026-04-23 15:53:12 +0200

    :sparkles: ajout route du détail livre et de la rerirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import {Nouveau} from './livres/component/nouveau/nouveau';
+import {Details} from './livres/component/details/details';
 
 export const routes: Routes = [
   { path: 'livres/nouveau', component: Nouveau },
+  { path: 'livres/:id', component: Details },
+  { path: '**', redirectTo: '' }
 ];

322e68fc08b4b76546a97f6ac411abf8bb1164a9
kimptgr <patingrekim@gmail.com>
2026-04-23 15:53:26 +0200

    :sparkles: ajout du toast

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import {Component, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {Navbar} from './commun/navbar/navbar';
 import {Footer} from './commun/footer/footer';
+import {Toast} from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, Toast],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

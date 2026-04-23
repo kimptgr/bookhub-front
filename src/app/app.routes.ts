641c6472053ae25ae23fe44d2209f8796565af15
mfuentes <fuentesmanuelfr@gmail.com>
2026-04-23 17:02:07 +0200

    :construction: [FEAT] US-AUTH-01 et US-AUTH-02 page de connexion et d'inscription  modification navbar  ajout des récupération de role et d'id avec le token JWT

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import {Nouveau} from './livres/component/nouveau/nouveau';
+import { Connexion } from './pages/connexion/connexion';
+import { Inscription } from './pages/inscription/inscription';
 import {Details} from './livres/component/details/details';
 
 export const routes: Routes = [
   { path: 'livres/nouveau', component: Nouveau },
   { path: 'livres/:id', component: Details },
   { path: '**', redirectTo: '' }
+  { path: 'connexion', component: Connexion },
+  { path: 'inscription', component: Inscription },
+  { path: '', redirectTo: 'connexion', pathMatch: 'full' }
 ];
---
+++ src/app/pages/connexion/connexion.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+// importe FormBuilder : pour créer un formulaire réactif, Validators : permet d'utiliser des validateurs existants,
+// ReactiveFormsModule : pour que le formulaire soit réactifs dans le HTML
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {Router, RouterLink} from '@angular/router';
+import { AuthService } from '../../services/authService';
+import { utilisateurService } from '../../services/utilisateurService';
+import { CommonModule } from '@angular/common';
+import {InputText} from 'primeng/inputtext';
+import {FloatLabel} from 'primeng/floatlabel';
+import {Password} from 'primeng/password';
+import {Button} from 'primeng/button';
+import {Message} from 'primeng/message';
+
+@Component({
+  selector: 'app-connexion',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, InputText, FloatLabel, Password, Button, Message, RouterLink],
+  templateUrl: './connexion.html',
+  styleUrl: './connexion.css',
+})
+export class Connexion {
+  form: FormGroup;
+  erreur: string = '';
+
+ // const role = this.utilisateurService.getRole();
+ // const id   = this.utilisateurService.getId();
+
+  // une instance du service, le service d'authentification et le service de navigation pour la redirection
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.form.invalid) return;
+
+    this.authService.connexion(this.form.value).subscribe({
+      next: () => this.router.navigate(['/livres/nouveau']),  // redirection après connexion TODO : modifier en fonction des rôles /catalogue
+      error: () => this.erreur = 'Email ou mot de passe incorrect'
+    });
+  }
+}
